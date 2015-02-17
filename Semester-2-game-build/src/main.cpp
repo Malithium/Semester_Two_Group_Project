fc4d0899f00e86255996ad16b7296249f0dbada7
Kyle Tuckey <Malithium@gmail.com>
2015-02-17 09:52:03 +0000

    Shader Starting Point
    Created some basic shaders, and a shader loader. It is not finalised yet
    as I need to test them at University.

--- Semester-2-game-build/src/main.cpp
+++ Semester-2-game-build/src/main.cpp
@@ -16,6 +16,8 @@ int main(int argc, char *argv[])
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
+	loadShaders shader;
+
 	//create SDL Window
 	SDL_Window* window = SDL_CreateWindow("OpenGL Window", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 
@@ -33,18 +35,70 @@ int main(int argc, char *argv[])
 	}
 
 	//initialise glew
-	 glewInit();
+	glewInit();
 	if (!glewIsSupported("GL_VERSION_3_0")) {
 		std::cerr << "OpenGL 3.0 not available" << std::endl;
-	return -1;
+		return -1;
 	}
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = shader.LoadShaders("VertexShader.vertexshader", "FragmentShader.fragmentshader");
+
+
+	// An array of 3 vectors which represents 3 vertices
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	// This will identify our vertex buffer
+	GLuint vertexbuffer;
+
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
 	//setup SDL windowEvent and game loop
 	SDL_Event windowEvent;
-	while(true){
-		if(SDL_PollEvent(&windowEvent))
+	while (true){
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+			);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+		glDisableVertexAttribArray(0);
+
+		if (SDL_PollEvent(&windowEvent))
 		{
-			if(windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+			if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
 		}
 		SDL_GL_SwapWindow(window);
 	}

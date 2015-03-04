d3e951f24b271436c381b9cf517687ae920b7772
Kyle Tuckey <Malithium@gmail.com>
2015-03-04 20:34:42 +0000

    Camera Bug fix's
    I have fixed the issue regarding Delta time, the mouse Issue is still
    ongoing, for whatever reason printing out the angle values somehow fixes
    the problem.

--- Semester-2-game-build/src/camera.cpp
+++ Semester-2-game-build/src/camera.cpp
@@ -4,16 +4,9 @@
 	glm::mat4 projectionMatrix;
 
 	//The variables we will be using to get the deltaTime
-	double currentTime = 0.0;
+	int currentTime = 0;
 	float d_time = 0.0f;
-	double previousTime = 0;
-
-	//The angles we will be using
-	float horizontalAngle = 0.0f;
-	float verticalAngle = 0.0f;
-
-	//our position vector
-	glm::vec3 position = glm::vec3(0,0,0);
+	int previousTime = 0;
 
 float Camera::deltaTime(){
 	currentTime = SDL_GetTicks();
@@ -32,15 +25,23 @@ glm::mat4 Camera::getViewMatrix(){
 
 /*
 *
-*@bug mouse movement lags the game out massively at the moment
-*
+*@bug 
+*for whatever reason, printing out the angle values fixes the lag issue
 */
 
+//The angles we will be using
+float horizontalAngle = 0.0f;
+float verticalAngle = 0.0f;
+
+//our position vector
+glm::vec3 position = glm::vec3(0,0,0);
+
 void Camera::cameraControls(SDL_Window* window, SDL_Event &event){
-	float speed = 1.0f;
-	float mouseSpeed = 0.003f;
+	float speed = 45.0f;
+	float mouseSpeed = 1.0f;
 	int xpos, ypos;
 	int midX, midY;
+	float dTime = deltaTime();
 
 	//store the screen's size into these variables;
 	SDL_GetWindowSize(window, &midX, &midY);
@@ -59,28 +60,35 @@ void Camera::cameraControls(SDL_Window* window, SDL_Event &event){
 	SDL_ShowCursor(SDL_DISABLE);
 
 	//calculate the screens new angles based on the mouse's position 
-	horizontalAngle += mouseSpeed * float(midX - xpos);
-	verticalAngle += mouseSpeed * float(midY - ypos);
+	horizontalAngle += mouseSpeed * dTime * float(midX - xpos);
+	verticalAngle += mouseSpeed * dTime * float(midY - ypos);
 
+	//stops the camera from going upside down
+	if (verticalAngle < -0.6f){
+		verticalAngle = -0.6f;
+	}
+	else if (verticalAngle > 0.6f){
+		verticalAngle = 0.6f;
+	}
 	
 	glm::vec3 direction(cos(verticalAngle) * sin(horizontalAngle), sin(verticalAngle), cos(verticalAngle) * cos(horizontalAngle));
 	glm::vec3 right = glm::vec3(sin(horizontalAngle) - 3.14f / 2, 0, cos(horizontalAngle) - 3.14f / 2);
 	glm::vec3 up = glm::cross(right, direction);
 
-	///detect wether the w,a,s,d keys have been pressed or are being held, and move the camera
+	//detect wether the w,a,s,d keys have been pressed or are being held, and move the camera
 	if (event.type == SDL_KEYDOWN){
 		switch (event.key.keysym.sym){
 		case SDLK_w:
-			position += direction * deltaTime() * speed;
+			position += direction * dTime * speed;
 			break;
 		case SDLK_s:
-			position -= direction * deltaTime() * speed;
+			position -= direction * dTime * speed;			
 			break;
 		case SDLK_d:
-			position += right * deltaTime() * speed;
+			position += right * dTime * speed;
 			break;
 		case SDLK_a:
-			position -= right * deltaTime() * speed;
+			position -= right * dTime * speed;
 			break;
 		}
 
@@ -90,6 +98,7 @@ void Camera::cameraControls(SDL_Window* window, SDL_Event &event){
 	projectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
 	viewMatrix = glm::lookAt(position, position + direction, up);
 
-}
 
-//dunjun
+	std::cout << verticalAngle << std::endl;
+	std::cout << horizontalAngle << std::endl;
+}

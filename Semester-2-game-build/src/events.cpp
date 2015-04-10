202af009c5d7ce079e2b80c6dfb56d987cb2a9fa
Kyle Tuckey <Malithium@gmail.com>
2015-04-10 15:16:56 +0100

    jump and time changes
    I have made some changes to how jumping and time works, they are not
    perfect currently but both are functional enough to give you a rough
    idea of what i am trying to go for.

--- Semester-2-game-build/src/events.cpp
+++ Semester-2-game-build/src/events.cpp
@@ -1,27 +1,16 @@
 #include "events.h"
 
 void Events::handleEvents(SDL_Event * event){
-	static double lastTime = SDL_GetTicks();
-	double currentTime = SDL_GetTicks();
-	float dTime = float(currentTime - lastTime) / 1000;
-	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL); //keyboard input handler
-
-	if (dTime > 0.150f)
-	{
-		dTime = 0.005f;
-	}
-
+	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+	
 	switch (event->type){
 	case SDL_MOUSEMOTION:
-		userInput.mouseMovement(dTime);
+		userInput.mouseMovement();
 		break;
 	}
 	
-	if (currentKeyStates[SDL_SCANCODE_W]){userInput.moveForward(dTime);}
-	else if (currentKeyStates[SDL_SCANCODE_S]){ userInput.moveBackward(dTime);}
-	else if (currentKeyStates[SDL_SCANCODE_SPACE]){ userInput.jumping(dTime);}
-	else if (currentKeyStates[SDL_SCANCODE_ESCAPE]){ std::exit(1); }
-	
-	userInput.jumping(dTime); // dTime made this complicated
-	lastTime = currentTime;
+	 if (currentKeyStates[SDL_SCANCODE_W]){userInput.moveForward();}
+     if (currentKeyStates[SDL_SCANCODE_S]){ userInput.moveBackward();}
+	 if (currentKeyStates[SDL_SCANCODE_SPACE]){ userInput.jumping();}
+	 if (currentKeyStates[SDL_SCANCODE_ESCAPE]){ std::exit(1); }
 }
